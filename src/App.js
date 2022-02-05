ea53170d7207b1fba9e3995683d2a0b9ff1e1f96
sgaurang82 <48280953+sgaurang82@users.noreply.github.com>
2022-02-05 08:21:53 -0500

    Corrected test case fail

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi ther 111e!!!
+          Learn React
         </a>
       </header>
     </div>
